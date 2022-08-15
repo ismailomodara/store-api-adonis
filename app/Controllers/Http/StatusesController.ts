7c4366c645fc253c537604dc34ddb9e99e6e10c0
Ismail Omodara <ismailomodara@Ismails-MacBook-Pro-2.local>
2022-08-15 22:25:58 +0100

    Created Status Controller/Model and migrated status table

---
+++ app/Controllers/Http/StatusesController.ts
@@ -0,0 +1,59 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Status from 'App/Models/Status'
+
+export default class StatusesController {
+  public async index() {
+    const statuses = await Status.all()
+    return {
+      status: true,
+      message: 'Statuses fetched',
+      data: statuses,
+    }
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const payload = request.body()
+    const status = await Status.create(payload)
+
+    return {
+      status: true,
+      message: 'Status created',
+      data: status,
+    }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const status = await Status.findOrFail(params.id)
+
+    return {
+      status: true,
+      message: 'Status fetched',
+      data: status,
+    }
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    let status = await Status.findOrFail(params.id)
+    status.name = request.body().name
+    status.label = request.body().label
+
+    await status.save()
+
+    return {
+      status: true,
+      message: 'Status updated',
+      data: status,
+    }
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    let status = await Status.findOrFail(params.id)
+    await status.delete()
+
+    return {
+      status: true,
+      message: 'Status deleted',
+      data: null,
+    }
+  }
+}
