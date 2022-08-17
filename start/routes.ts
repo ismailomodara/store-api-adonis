a54b1b96761d8612051a6057f5e545eda0fe508b
Ismail Omodara <ismailomodara@Ismails-MacBook-Pro-2.local>
2022-08-17 22:23:28 +0100

    Setup Model/Db for Status/Company

--- start/routes.ts
+++ start/routes.ts
@@ -4,7 +4,7 @@
 |--------------------------------------------------------------------------
 |
 | This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
+| for the majority of projects, however you can define routes in different
 | files and just make sure to import them inside this file. For example
 |
 | Define routes in following two files
@@ -24,4 +24,4 @@ Route.get('/', async () => {
   return { hello: 'world' }
 })
 
-Route.resource('/statuses', 'StatusesController').apiOnly()
+Route.resource('/api/v1/statuses', 'StatusesController').apiOnly()
