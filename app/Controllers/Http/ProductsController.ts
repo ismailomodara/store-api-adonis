18e7c5dff9c35f527bb9cd7e4dda401877b7d3c6
Ismail Omodara <ismailomodara@Ismails-MacBook-Pro-2.local>
2022-08-21 09:50:18 +0100

    Completed Products Query with multiple properties

--- app/Controllers/Http/ProductsController.ts
+++ app/Controllers/Http/ProductsController.ts
@@ -2,18 +2,64 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Product from 'App/Models/Product'
 import { DateTime } from 'luxon'
 
-const products = require('../../../products.json')
+// Used to populate the products db
+// const products = require('../../../products.json')
 
 export default class ProductsController {
-  public async index() {
-    const products = await Product.all()
+  public async index({ request }: HttpContextContract) {
+    const Products = Product.query()
+
+    /**
+     * Always returned products that are not deleted [status_id === 3]
+     */
+    Products.where('status_id', '!=', 3)
+
+    const { page, limit, order, range, fields } = request.qs()
+    const pageInt = Number(page) || 1
+    const limitInt = Number(limit) || 10
+
+    /**
+     * Filter [Where] Properties
+     */
+    if (range) {
+      const regEx = /\b(>|>=|=|<|<=)/g
+      let rangeFilters = range.replace(regEx, (match) => `_${match}_`)
+
+      const options = ['price', 'rating']
+
+      rangeFilters.split(',').forEach((property) => {
+        const [key, operator, value] = property.split('_')
+
+        if (options.includes(key)) {
+          Products.where(key, operator, value)
+        }
+      })
+    }
+    /**
+     * Order Properties
+     */
+    let orderBy = []
+    if (order) {
+      orderBy = order.split(',').map((property) => {
+        return {
+          column: property.charAt(0) === '-' ? property.slice(1) : property,
+          order: property.charAt(0) === '-' ? 'desc' : 'asc',
+        }
+      })
+    }
+
+    let data = await Products.orderBy(orderBy).paginate(pageInt, limitInt)
+
+    const products = fields
+      ? data.serialize({
+          fields: fields.split(','),
+        })
+      : data
+
     return {
       status: true,
       message: 'Products fetched',
-      data: {
-        total: products.length,
-        products,
-      },
+      data: products,
     }
   }
 
@@ -32,22 +78,26 @@ export default class ProductsController {
     }
   }
 
-  public async storeMany({ request }: HttpContextContract) {
-    console.log(request.body())
-    const productsPayload = products.map((product) => {
-      return {
-        ...product,
-        statusId: Math.floor(Math.random() * 3),
-      }
-    })
-    const newProducts = await Product.createMany(productsPayload)
-
-    return {
-      status: true,
-      message: 'Product created',
-      data: newProducts,
-    }
-  }
+  /**
+   * Used to populate the products db
+   * @param params
+   */
+  // public async storeMany({ request }: HttpContextContract) {
+  //   console.log(request.body())
+  //   const productsPayload = products.map((product) => {
+  //     return {
+  //       ...product,
+  //       statusId: Math.floor(Math.random() * 3),
+  //     }
+  //   })
+  //   const newProducts = await Product.createMany(productsPayload)
+  //
+  //   return {
+  //     status: true,
+  //     message: 'Product created',
+  //     data: newProducts,
+  //   }
+  // }
 
   public async show({ params }: HttpContextContract) {
     const product = await Product.findOrFail(params.id)
@@ -62,6 +112,7 @@ export default class ProductsController {
   public async update({ params, request }: HttpContextContract) {
     let product = await Product.findOrFail(params.id)
     product.name = request.body().name
+    product.statusId = request.body().status_id
 
     await product.save()
 
@@ -77,6 +128,8 @@ export default class ProductsController {
 
     product.statusId = 3
     product.deletedAt = DateTime.now()
+
+    await product.save()
 
     return {
       status: true,
